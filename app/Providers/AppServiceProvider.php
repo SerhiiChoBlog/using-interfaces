e22333406638c2b37a3e1ee350d6a01737a81de1
SerhiiCho <serhiicho@protonmail.com>
2022-07-22 14:54:58 +0300

    Changed binding from Twilio to Logger SMS service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\SmsServices\LoggerSmsService;
 use App\SmsServices\SmsService;
-use App\SmsServices\TwilioSmsService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->app->bind(SmsService::class, TwilioSmsService::class);
+        $this->app->bind(SmsService::class, LoggerSmsService::class);
     }
 }
