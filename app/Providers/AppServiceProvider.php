eac221a181061e98f031eeadc516574ff35cf21b
SerhiiCho <serhiicho@protonmail.com>
2022-07-22 14:53:10 +0300

    Added binding to AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,28 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use App\SmsServices\SmsService;
+use App\SmsServices\TwilioSmsService;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
+        $this->app->bind(SmsService::class, TwilioSmsService::class);
     }
 }
